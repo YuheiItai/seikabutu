206657e4a0a5162d037dac121ae2ed105a74d62c
YuheiItai <yuuhei1467@gmail.com>
2024-01-18 15:20:12 +0900

    abc

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>オススメの大分温泉</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>温泉</h2>
+                <input type="text" name="post[title]" placeholder="⚪︎⚪︎温泉" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>オススメポイント</h2>
+                <textarea name="post[body]" placeholder="広い"></textarea>
+            </div>
+            <div class="body">
+                <h2>こんな人にオススメ</h2>
+                <textarea name="post[body]" placeholder="ゆっくりしたい人">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="投稿"/>
+        </form>
+        <div class="ホームページへ戻る">[<a href="/">戻る</a>]</div>
+    </body>
+</html>
